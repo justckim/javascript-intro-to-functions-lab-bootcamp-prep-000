28cbfce8e74b4184280ac27767779bbbc694e11a
Justin <jkimm516@gmail.com>
2018-04-17 06:25:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   
   if (string.toLowerCase === whisper(string)){
-    console.log("I can't hear you!")
+    return "I can't hear you!"
   }
   
   else if (string.toUpperCase === shout(string)){
